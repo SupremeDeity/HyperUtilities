98dacc002a507da6536e6217265f853ee846fc03
SupremeDeity <sheikhmohsin706@gmail.com>
2020-04-23 23:45:26 +0500

    Fixed Bot

--- bot.js
+++ bot.js
@@ -1,29 +1,30 @@
-const Discord = require('discord.js');
-
+// This line MUST be first, for discord.js to read the process envs!
+require('dotenv').config(); 
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
- 
+client.on("message", message => {
+  if (message.author.bot) return;
+  // The process.env.PREFIX is your bot's prefix in this case.
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
 
-client.on('message', message => {
+  // This is the usual argument parsing we love to use.
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+  // And our 2 real basic commands!
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
+  }
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+// There's zero need to put something here. Discord.js uses process.env.CLIENT_TOKEN if it's available,
+// and this is what is being used here. If on discord.js v12, it's DISCORD_TOKEN
+client.login();
